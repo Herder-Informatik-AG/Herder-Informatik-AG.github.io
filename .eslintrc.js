53d941035211944b1c0c411127fd62a0fb6e28d1
JulianEgbert <Julian.Egbert@student.hpi.de>
2021-07-09 21:12:50 +0200

    Start Refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,18 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ['plugin:vue/essential', '@vue/standard', 'prettier'],
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
     parserOptions: {
-        parser: 'babel-eslint',
+        ecmaVersion: 2020,
     },
 };
